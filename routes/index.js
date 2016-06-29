23e822a49b6ac9a0e6dc0c385260931d8ccfe570
Marcel Melo <marcel.msmelo@gmail.com>
2016-06-29 14:50:32 -0300

    post form

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {data: req.query});
 });
 
 router.post('/', function(req, res, next){
-  console.log('BODY', req.body);
   res.render('formPost', {dataForm: req.body});
 });
 
