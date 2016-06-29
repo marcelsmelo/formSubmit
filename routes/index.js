7f0e6bdd528f0b5ddb3a2a3085e845ea74086c4b
Marcel Melo <marcel.msmelo@gmail.com>
2016-06-29 14:08:27 -0300

    Project create and first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {data: req.query});
+});
+
+router.post('/', function(req, res, next){
+  res.render('index', {data: req.body});
+});
+
+module.exports = router;
